8f925c516703b037a3a5fbc380257556544b1b0b
lemonbrass <shebangsache@gmai.com>
2026-05-12 12:42:39 +0000

    Fixed a bug in pp2_cond.c where the #else branch wasnt parsed correctly and cleaned up

--- cli/main.c
+++ cli/main.c
@@ -1,14 +1,14 @@
-#include <thirdparty/kvec.h>
-#include <token.h>
-#include <token_source.h>
 #include <compiler.h>
 #include <da_string.h>
 #include <setjmp.h>
 #include <stdio.h>
+#include <thirdparty/kvec.h>
+#include <token.h>
+#include <token_source.h>
 
 int main() {
   jmp_buf onerror;
-  CompilerOpt* opt = new_opt();
+  CompilerOpt *opt = new_opt();
   opt_include_dir(opt, str_from_cstr_copy("."));
   ChucciCompiler ctx = new_compiler(opt, str_from_cstr_copy(""), &onerror);
   da_string dsinput = new_ds();
@@ -18,27 +18,30 @@ int main() {
       printf("chuccic> ");
       int ch;
       while (!s_endswith(ds_to_sv(&dsinput), ";;\n")) {
-        if (dsinput.len != 0) printf("...> ");
+        if (dsinput.len != 0)
+          printf(".......> ");
         while ((ch = getchar()) != EOF && ch != '\n') {
           push_char_ds(&dsinput, (unsigned char)ch);
         }
+        if (s_eq(ds_to_sv(&dsinput), sv_from_cstr("exit")))
+          goto end;
         push_char_ds(&dsinput, '\n');
       }
       dsinput.len -= 3; // remove the ;;\n
-      if (s_eq(ds_to_sv(&dsinput), sv_from_cstr("exit"))) break;
+      if (s_eq(ds_to_sv(&dsinput), sv_from_cstr("exit")))
+        break;
       push_char_ds(&dsinput, '\0');
       string source = ds_to_str_copy(&dsinput);
       add_source(&ctx, source);
-      if (s_eq(source, sv_from_cstr("exit"))) break;
 
       TokenArray result = compiler_compile(&ctx);
       print_token_array(&result);
       kv_destroy(result);
     }
+  end:
     free_ds(&dsinput);
     free_compiler(&ctx);
-  }
-  else {
+  } else {
     free_ds(&dsinput);
     free_compiler(&ctx);
     return 1;
--- libchucci/src/pp2_cond.c
+++ libchucci/src/pp2_cond.c
@@ -1,31 +1,33 @@
-#include <thirdparty/kvec.h>
-#include <pp2_macro.h>
+#include <assert.h>
+#include <ctype.h>
 #include <da_internmap.h>
 #include <da_string.h>
-#include <assert.h>
+#include <pp2_cond.h>
+#include <pp2_macro.h>
+#include <preprocess_2.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <thirdparty/kvec.h>
 #include <token.h>
 #include <token_source.h>
-#include <pp2_cond.h>
-#include <preprocess_2.h>
-#include <ctype.h>
-#include <stddef.h>
 
+int eval_cond(Preprocessor2 *pp2, int minbp);
 
-int eval_cond(Preprocessor2* pp2, int minbp);
-
-int get_bp(Preprocessor2* pp2, Token token) {
-  #define X(op, b, bp) if (token.kind == op) return bp;
+int get_bp(Preprocessor2 *pp2, Token token) {
+#define X(op, b, bp)                                                           \
+  if (token.kind == op)                                                        \
+    return bp;
   PP_INFIX_OPERATORS(X)
   PP_PREFIX_OPERATORS(X)
   PP_PAREN_OPERATOR(X)
   PP_TERNARY_OPERATOR(X)
-  #undef X
+#undef X
   throw_error(pp2->token_source, token, "Unexpected token", pp2->ctx);
   assert(false && "UNREACHABLE");
 }
 
-// TODO: Segregate string and numerical tokens, and parse string->number in lexer
+// TODO: Segregate string and numerical tokens, and parse string->number in
+// lexer
 int strntoi(const char *s, size_t n) {
   const char *p = s;
   const char *end = s + n;
@@ -42,7 +44,7 @@ int strntoi(const char *s, size_t n) {
   return digits ? result : 0;
 }
 
-int nud(Preprocessor2* pp2, Token token) {
+int nud(Preprocessor2 *pp2, Token token) {
   // Number
   if (token.kind == TOK_VAL && token.val.cstr[0] != '"') {
     return strntoi(token.val.cstr, token.val.len);
@@ -54,7 +56,8 @@ int nud(Preprocessor2* pp2, Token token) {
       expect_token_kind(pp2->token_source, SEP_RPAREN, pp2->ctx);
       return imap_has(pp2->ctx->macros, macro.ident);
     }
-    if (!imap_has(pp2->ctx->macros, token.ident)) return 0;
+    if (!imap_has(pp2->ctx->macros, token.ident))
+      return 0;
     TokenArray buf = {0};
     macro_use(pp2, &token, &buf);
     TokenSource ts = ts_from_array(buf);
@@ -63,9 +66,11 @@ int nud(Preprocessor2* pp2, Token token) {
     tmp.token_source = &ts;
     return eval_cond(&tmp, 0);
   }
-  #define X(opkind, op, bp) if (opkind == token.kind) return op eval_cond(pp2, bp);
+#define X(opkind, op, bp)                                                      \
+  if (opkind == token.kind)                                                    \
+    return op eval_cond(pp2, bp);
   PP_PREFIX_OPERATORS(X)
-  #undef X
+#undef X
   if (SEP_LPAREN == token.kind) {
     int res = eval_cond(pp2, 0);
     expect_token_kind(pp2->token_source, SEP_RPAREN, pp2->ctx);
@@ -75,10 +80,12 @@ int nud(Preprocessor2* pp2, Token token) {
   assert(false && "UNREACHABLE");
 }
 
-int led(Preprocessor2* pp2, Token token, int left) {
-  #define X(opkind, op, bp) if (opkind == token.kind) return left op eval_cond(pp2, bp);
+int led(Preprocessor2 *pp2, Token token, int left) {
+#define X(opkind, op, bp)                                                      \
+  if (opkind == token.kind)                                                    \
+    return left op eval_cond(pp2, bp);
   PP_INFIX_OPERATORS(X)
-  #undef X
+#undef X
   if (OP_QUESTION == token.kind) {
     int then = eval_cond(pp2, 0);
     expect_token_kind(pp2->token_source, OP_COLON, pp2->ctx);
@@ -89,15 +96,18 @@ int led(Preprocessor2* pp2, Token token, int left) {
   assert(false && "UNREACHABLE");
 }
 
-int eval_cond(Preprocessor2* pp2, int minbp) {
+int eval_cond(Preprocessor2 *pp2, int minbp) {
   int left = nud(pp2, next_token(pp2->token_source));
 
   while (true) {
     Token op = peek_token(pp2->token_source);
-    if (op.kind == TOK_EOF || op.kind == SEP_RPAREN || op.kind == OP_COLON || op.kind == SEP_NEWLINE) break;
+    if (op.kind == TOK_EOF || op.kind == SEP_RPAREN || op.kind == OP_COLON ||
+        op.kind == SEP_NEWLINE)
+      break;
     int bp = get_bp(pp2, op);
-    
-    if (bp <= minbp) break;
+
+    if (bp <= minbp)
+      break;
     next_token(pp2->token_source); // skip peeked token
 
     left = led(pp2, op, left);
@@ -105,47 +115,69 @@ int eval_cond(Preprocessor2* pp2, int minbp) {
   return left;
 }
 
-void cond_use(Preprocessor2* pp2) {
+void push_body(Preprocessor2 *pp2) {
+  Token next;
+  bool skip = false;
+  TokenArray buf = {0};
+  while ((next = next_token(pp2->token_source), true)) {
+    if (next.kind == TOK_EOF)
+      throw_error(pp2->token_source, next, "Unexpected EOF", pp2->ctx);
+    if (next.kind == OP_PREPROCESS) {
+      Token op = peek_token(pp2->token_source);
+      if (op.kind == TOK_IDENT && s_eq(op.ident, sv_from_cstr("endif"))) {
+        next_token(pp2->token_source);
+        break;
+      }
+      if (op.kind == TOK_IDENT && s_eq(op.ident, sv_from_cstr("elif")))
+        skip = true;
+      if (op.kind == KW_ELSE)
+        skip = true;
+    }
+    if (!skip)
+      kv_push(Token, buf, next);
+  }
+  TokenSource src = ts_from_array(buf);
+  TokenArray result_tokens = recursively_expand(pp2, &src);
+  kv_push_vec(Token, pp2->stream, result_tokens);
+  kv_destroy(buf);
+  kv_destroy(result_tokens);
+}
+
+void cond_use(Preprocessor2 *pp2) {
   int res = eval_cond(pp2, 0);
   if (res) {
+    push_body(pp2);
+  } else {
     Token next;
     TokenArray buf = {0};
-    bool skip = false;
+    bool skip = true;
     while ((next = next_token(pp2->token_source)), true) {
+      if (next.kind == TOK_EOF)
+        throw_error(pp2->token_source, next, "Unexpected EOF", pp2->ctx);
       if (next.kind == OP_PREPROCESS) {
-        Token next = peek_token(pp2->token_source);
-        if (next.kind == KW_ELSE) skip = true;
-        if (next.kind == TOK_IDENT && s_eq(next.ident, sv_from_cstr("endif"))) {
+        Token op = peek_token(pp2->token_source);
+        if (op.kind == TOK_IDENT && s_eq(op.ident, sv_from_cstr("endif"))) {
           next_token(pp2->token_source);
           break;
         }
-        if (next.kind == TOK_IDENT && s_eq(next.ident, sv_from_cstr("elif"))) skip = true;
-      }
-      if (next.kind == TOK_EOF) throw_error(pp2->token_source, next, "Unexpected token", pp2->ctx);
-      if (!skip) kv_push(Token, buf, next);
-    }
-    TokenSource ts = ts_from_array(buf);
-    TokenArray result_tokens = recursively_expand(pp2, &ts);  
-    kv_push_vec(Token, pp2->stream, result_tokens);
-    kv_destroy(result_tokens);
-    kv_destroy(buf);
-  }
-  else {
-    Token next;
-    bool else_body = false;
-    while ((next = next_token(pp2->token_source)), true) {
-      if (next.kind == OP_PREPROCESS) {
-        next = next_token(pp2->token_source);
-        if (next.kind == TOK_IDENT && s_eq(next.ident, sv_from_cstr("endif"))) break;
-        else if (next.kind == TOK_IDENT && s_eq(next.ident, sv_from_cstr("elif"))) { 
+        if (op.kind == TOK_IDENT && s_eq(op.ident, sv_from_cstr("elif"))) {
+          next_token(pp2->token_source);
           cond_use(pp2);
           break;
         }
-        else if (next.kind == KW_ELSE) else_body = true;
-        else throw_error(pp2->token_source, next, "Unexpected token", pp2->ctx);
+        if (op.kind == KW_ELSE) {
+          next_token(pp2->token_source);
+          skip = false;
+          continue;
+        }
       }
-      if (next.kind == TOK_EOF) throw_error(pp2->token_source, next, "Unexpected token", pp2->ctx);
-      if (else_body) kv_push(Token, pp2->stream, next);
+      if (!skip)
+        kv_push(Token, buf, next);
     }
+    TokenSource src = ts_from_array(buf);
+    TokenArray result_tokens = recursively_expand(pp2, &src);
+    kv_push_vec(Token, pp2->stream, result_tokens);
+    kv_destroy(buf);
+    kv_destroy(result_tokens);
   }
 }
